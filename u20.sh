268802bd7912e82bdf1f58fa1000aa28b30df9d4
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2021-02-16 13:36:00 +0100

    u20: improvements reported by Felix

--- u20.sh
+++ u20.sh
@@ -6,12 +6,12 @@
 #
 #  This file is part of U20 proyect.
 #
-#  DaLoFlow is free software: you can redistribute it and/or modify
+#  U20 is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  WepSIM is distributed in the hope that it will be useful,
+#  U20 is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
