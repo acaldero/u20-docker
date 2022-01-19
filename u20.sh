9cc9df7006a57dedaefe6a2c94bd43ee5bcaa22d
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2022-01-19 11:16:47 +0100

    minor update for docker 4.x

--- u20.sh
+++ u20.sh
@@ -52,7 +52,8 @@ fi
 # for each argument, try to execute it
 #
 
-DOCKER_PREFIX_NAME=docker.node.
+#DOCKER_PREFIX_NAME=docker.node.
+ DOCKER_PREFIX_NAME=$(basename $(pwd))"-node"
 
 while (( "$#" ))
 do
